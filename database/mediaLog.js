0e61255a9c0d0883f32548d29961c609f9c91385
fergmac <f.macconnell@gmail.com>
2018-12-12 21:35:09 -0800

    Create db models - in progress

---
+++ database/mediaLog.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const MediaLogSchema = new Schema({
+    // TODO: create mediaObject
+    // id
+    // name
+    // uploadedBy
+    // metaData ( mp3 meta data )
+});
---
+++ test/create_test.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+const User = require('../database/user.js');
+
+describe('Creating records', () => {
+    it('saves a user', (done) => {
+        // Creating new instance of User
+        const user = new User({ name: 'Ferg' });
+
+        // Ferg the object has a bunch of functions attached to it now
+        // async - will take time - saves Ferg to db
+        user.save()
+            .then(() => {
+                // Has Ferg been saved successfully?
+                assert(!user.isNew);
+                done();
+            });
+    });
+});
