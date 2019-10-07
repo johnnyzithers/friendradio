26bfa762d9341b583a253f4ed9bdf01f2d535a6a
dmacconnell <duncanmacconnell@gmail.com>
2019-10-07 14:11:48 -0400

    heroku db

--- src/start.js
+++ src/start.js
@@ -67,7 +67,7 @@ var storage = multer.diskStorage({
   }
 })
 
-const PORT = 3001
+// const PORT = 3001
 const MONGO_URL = 'mongodb://heroku_csm1p15v:rn77mngmt257a8qvjk7df0u798@ds331548.mlab.com:31548/heroku_csm1p15v'
 const HLS_UPLOAD_DIR = './uploads/hls/';
 const UPLOAD_PATH = './uploads';
@@ -106,155 +106,156 @@ initRooms();
 export const start = async () => {
   try 
   {
-    // reference to the mongo database
-    db = await MongoClient.connect(MONGO_URL);
+    // // reference to the mongo database
+    // db = await MongoClient.connect(MONGO_URL);
 
-    // route for streaming track by ID
-    trackRoute.get('/:trackID', (req, res) => {
-      // FIXME 
-      console.log("streamTrackNum: " + streamTrackNum);
-      try {
-        var trackID = new ObjectID(req.params.trackID);
-      } catch(err) {
-        console.log(err);
-        return res.status(400).json({ message: "Invalid trackID in URL parameter. Must be a single String of 12 bytes or a string of 24 hex characters" }); 
-      }
+    // // route for streaming track by ID
+    // trackRoute.get('/:trackID', (req, res) => {
+    //   // FIXME 
+    //   console.log("streamTrackNum: " + streamTrackNum);
+    //   try {
+    //     var trackID = new ObjectID(req.params.trackID);
+    //   } catch(err) {
+    //     console.log(err);
+    //     return res.status(400).json({ message: "Invalid trackID in URL parameter. Must be a single String of 12 bytes or a string of 24 hex characters" }); 
+    //   }
 
-      let bucket = new mongodb.GridFSBucket(db, {
-        bucketName: 'track'+streamTrackNum
-      });
+    //   let bucket = new mongodb.GridFSBucket(db, {
+    //     bucketName: 'track'+streamTrackNum
+    //   });
 
-      // set the appropriate mongo collections
-      const collection = db.collection('track'+streamTrackNum+'.files');    
+    //   // set the appropriate mongo collections
+    //   const collection = db.collection('track'+streamTrackNum+'.files');    
 
-      // find all files
-      collection.find({}).toArray((err, files) => {
-        // Error checking
-        if(!files || files.length === 0){
-            return res.status(404).json({
-                responseCode: 1,
-                responseMessage: "error"
-            });
-        }
-        // Loop through all the files and fetch the necessary information
-        files.forEach((file) => {
+    //   // find all files
+    //   collection.find({}).toArray((err, files) => {
+    //     // Error checking
+    //     if(!files || files.length === 0){
+    //         return res.status(404).json({
+    //             responseCode: 1,
+    //             responseMessage: "error"
+    //         });
+    //     }
+    //     // Loop through all the files and fetch the necessary information
+    //     files.forEach((file) => {
           
-          bucket.openDownloadStreamByName(file.filename)
-          .pipe(fs.createWriteStream('./tmp/track'+streamTrackNum+'/'+file.filename))
-          .on('error', function(error) {
-            assert.ifError(error);
-          })
-          .on('finish', function() {
-              // fired for each file
-          });       
-        });
-        streamTrackNum = streamTrackNum + 1;
-        res.json(files);
-        return res;
-      });
-    });
+    //       bucket.openDownloadStreamByName(file.filename)
+    //       .pipe(fs.createWriteStream('./tmp/track'+streamTrackNum+'/'+file.filename))
+    //       .on('error', function(error) {
+    //         assert.ifError(error);
+    //       })
+    //       .on('finish', function() {
+    //           // fired for each file
+    //       });       
+    //     });
+    //     streamTrackNum = streamTrackNum + 1;
+    //     res.json(files);
+    //     return res;
+    //   });
+    // });
 
-    // upload tracks 
-    trackRoute.post('/:roomNum', (req, res) => {
-     try {
-        var rID = req.params.roomNum;
-      } catch(err) {
-        console.log(err);
-        return res.status(400).json({ message: "Invalid room num in URL parameter. Must be a single String of 12 bytes or a string of 24 hex characters" }); 
-      }
+    // // upload tracks 
+    // trackRoute.post('/:roomNum', (req, res) => {
+    //  try {
+    //     var rID = req.params.roomNum;
+    //   } catch(err) {
+    //     console.log(err);
+    //     return res.status(400).json({ message: "Invalid room num in URL parameter. Must be a single String of 12 bytes or a string of 24 hex characters" }); 
+    //   }
 
-      const upload = multer({ storage: storage, limits: { fields: 1, fileSize: 9000000, files: 1, parts: 2}});
+    //   const upload = multer({ storage: storage, limits: { fields: 1, fileSize: 9000000, files: 1, parts: 2}});
 
-      upload.single('track')(req, res, (err) => {  
-        if (err) {
-          console.log(err); 
-          return res.status(400).json({ message: "Upload Request Validation Failed" });
-        } else if (!req.body.name) {
-          return res.status(400).json({ message: "No track name in request body" });
-        } else if (typeof(req.file) == 'undefined'){
-          return res.status(400).json({ message: "File not found. Are you sure you selected a file? "});
-        } else {
-          console.log("no upload error");
-        }
+    //   upload.single('track')(req, res, (err) => {  
+    //     if (err) {
+    //       console.log(err); 
+    //       return res.status(400).json({ message: "Upload Request Validation Failed" });
+    //     } else if (!req.body.name) {
+    //       return res.status(400).json({ message: "No track name in request body" });
+    //     } else if (typeof(req.file) == 'undefined'){
+    //       return res.status(400).json({ message: "File not found. Are you sure you selected a file? "});
+    //     } else {
+    //       console.log("no upload error");
+    //     }
           
-        // path of mp3 on server, for ffmpeg
-        let mp3Path = req.file.path;
+    //     // path of mp3 on server, for ffmpeg
+    //     let mp3Path = req.file.path;
 
-        // use multer storage filename as mongo file names
-        let filename = req.file.path.replace(/^.*[\\\/]/, '');
+    //     // use multer storage filename as mongo file names
+    //     let filename = req.file.path.replace(/^.*[\\\/]/, '');
 
-        // get reference to gridfs
-        let bucket = new mongodb.GridFSBucket(db, {
-          bucketName: 'track'+uploadTrackNum
-        });
-        uploadTrackNum = uploadTrackNum + 1;
-        // callback function for saving ffmpeg files to db
-        function trackStorageCallback()
-        {
+    //     // get reference to gridfs
+    //     let bucket = new mongodb.GridFSBucket(db, {
+    //       bucketName: 'track'+uploadTrackNum
+    //     });
+    //     uploadTrackNum = uploadTrackNum + 1;
+    //     // callback function for saving ffmpeg files to db
+    //     function trackStorageCallback()
+    //     {
 
-          fs.readdir(`${HLS_UPLOAD_DIR}`,function(err,files){
-            if(err) throw err;
-            var count = 0;
+    //       fs.readdir(`${HLS_UPLOAD_DIR}`,function(err,files){
+    //         if(err) throw err;
+    //         var count = 0;
 
-            files.forEach(function(file){
+    //         files.forEach(function(file){
               
-              // FIXME filtering out ds_store
-              if (file == ".DS_Store"){
-                console.log("trackStorageCallback() -- filtering .DS_Store...");
-              // }
-              // else if (file == "output.m3u8") {
+    //           // FIXME filtering out ds_store
+    //           if (file == ".DS_Store"){
+    //             console.log("trackStorageCallback() -- filtering .DS_Store...");
+    //           // }
+    //           // else if (file == "output.m3u8") {
 
-              } else {``
-                // stream the file to database using gridfs
-                fs.createReadStream(`${HLS_UPLOAD_DIR}`+file)
-                .pipe(bucket.openUploadStream(file))
-                .on('error', ()=>{
-                    console.log("Some error occured:"+error);
-                    res.send(error);
-                })
-                .on('finish', ()=>{
-                    // FIXME validate all the files arrived
-                });
-              }
-            });
-          });
-          res.status(201).json({ message: "HLS files successfully uploaded to mongo: "});
+    //           } else {``
+    //             // stream the file to database using gridfs
+    //             fs.createReadStream(`${HLS_UPLOAD_DIR}`+file)
+    //             .pipe(bucket.openUploadStream(file))
+    //             .on('error', ()=>{
+    //                 console.log("Some error occured:"+error);
+    //                 res.send(error);
+    //             })
+    //             .on('finish', ()=>{
+    //                 // FIXME validate all the files arrived
+    //             });
+    //           }
+    //         });
+    //       });
+    //       res.status(201).json({ message: "HLS files successfully uploaded to mongo: "});
           
-          // add this track to the room playlist
-          // FIXME better way to do this
-          var room = rooms.find(function(r) {
-            return r.roomID ==  rID;
-          });
-          room.playlist.push({ uri: filename, name: req.body.name });
-          console.log("adding "+ req.body.name + " to room " + room.roomID);
+    //       // add this track to the room playlist
+    //       // FIXME better way to do this
+    //       var room = rooms.find(function(r) {
+    //         return r.roomID ==  rID;
+    //       });
+    //       room.playlist.push({ uri: filename, name: req.body.name });
+    //       console.log("adding "+ req.body.name + " to room " + room.roomID);
           
-          // remove tempory files once mongo upload complete
-          removeAllFilesFromDir("hls");
-          removeAllFilesFromDir("mp3");
+    //       // remove tempory files once mongo upload complete
+    //       removeAllFilesFromDir("hls");
+    //       removeAllFilesFromDir("mp3");
 
-          // and return the response
-          return res;
-        }
+    //       // and return the response
+    //       return res;
+    //     }
 
-        // ffmpeg call to create the .m3u8 and .ts segments from mp3
-        ffmpeg(mp3Path, { timeout: 432000 }).addOptions([
-              '-profile:v baseline', // baseline profile (level 3.0) for H264 video codec
-              '-level 3.0', 
-              '-s 640x360',          // 640px width, 360px height output video dimensions
-              '-start_number 0',     // start the first .ts segment at index 0
-              '-hls_time 10',        // 10 second segment duration
-              '-hls_list_size 0',    // Maxmimum number of playlist entries (0 means all entries/infinite)
-              '-f hls'               // HLS format
-          ])
-          .audioCodec('libmp3lame')
-          .on('error', function(err, stdout, stderr) {
-            console.log('Error: ' + err.message);
-            console.log('ffmpeg output:\n' + stdout);
-            console.log('ffmpeg stderr:\n' + stderr);
-          })
-          .output(`${HLS_UPLOAD_DIR}`+filename+`.m3u8`).on('end', trackStorageCallback).run() 
-      });
-    });
+    //     // ffmpeg call to create the .m3u8 and .ts segments from mp3
+    //     ffmpeg(mp3Path, { timeout: 432000 }).addOptions([
+	
+    //           '-profile:v baseline', // baseline profile (level 3.0) for H264 video codec
+    //           '-level 3.0', 
+    //           '-s 640x360',          // 640px width, 360px height output video dimensions
+    //           '-start_number 0',     // start the first .ts segment at index 0
+    //           '-hls_time 10',        // 10 second segment duration
+    //           '-hls_list_size 0',    // Maxmimum number of playlist entries (0 means all entries/infinite)
+    //           '-f hls'               // HLS format
+    //       ])
+    //       .audioCodec('libmp3lame')
+    //       .on('error', function(err, stdout, stderr) {
+    //         console.log('Error: ' + err.message);
+    //         console.log('ffmpeg output:\n' + stdout);
+    //         console.log('ffmpeg stderr:\n' + stderr);
+    //       })
+    //       .output(`${HLS_UPLOAD_DIR}`+filename+`.m3u8`).on('end', trackStorageCallback).run() 
+    //   });
+    // });
   } catch (e) {
     console.log(e)
   }
